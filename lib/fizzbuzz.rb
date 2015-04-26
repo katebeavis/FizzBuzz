70fe91d2b946f13455afe9eb86298f4090dd1ae7
katebeavis <katebeavis84@gmail.com>
2015-04-26 14:08:34 +0100

    Added travis file and gemfile

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,34 +1,32 @@
 class Fizzbuzz
+  def not_divisible_by_three(number)
+    number % 3 == 0
+  end
 
-	def is_not_divisible_by_three(number)
-		number % 3 == 0
-	end
+  def divisible_by_three(number)
+    number % 3 == 0
+  end
 
-	def is_divisible_by_three(number)
-		number % 3 == 0
-	end
+  def not_divisible_by_five(number)
+    number % 5 == 0
+  end
 
-	def is_not_divisible_by_five(number)
-		number % 5 == 0
-	end
+  def divisible_by_five(number)
+    number % 5 == 0
+  end
 
-	def is_divisible_by_five(number)
-		number % 5 == 0
-	end
+  def not_divisible_by_fifteen(number)
+    number % 15 == 0
+  end
 
-	def is_not_dividible_by_fifteen(number)
-		number % 15 == 0
-	end
+  def divisible_by_fifteen(number)
+    number % 15 == 0
+  end
 
-	def is_divisible_by_fifteen(number)
-		number % 15 == 0
-	end
-
-	def playing(number)
-		return "Fizzbuzz" if is_divisible_by_fifteen(number)
-		return "Buzz" if is_divisible_by_five(number)
-		return "Fizz" if is_divisible_by_three(number)
-		number
-	end
-
+  def playing(number)
+    return 'Fizzbuzz' if divisible_by_fifteen(number)
+    return 'Buzz' if divisible_by_five(number)
+    return 'Fizz' if divisible_by_three(number)
+    number
+  end
 end
